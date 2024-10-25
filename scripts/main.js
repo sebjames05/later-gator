9572d58aa3c71327a51aac1a5d422e886a634fb4
sebjames05 <sebjames.00@gmail.com>
2024-10-24 22:52:33 -0400

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -28,15 +28,15 @@ input.addEventListener("keydown", (ev) => {
     console.log("Enter detected. current value:", input.value);
     // TODO use the provided later() function here
     options((keys) => {
-      console.log("here");
       for (let i = 0; i < keys.length; i++) {
-        console.log(keys[i]);
         const newButton = document.createElement('button');
         newButton.textContent = keys[i];
+        newButton.addEventListener('click', () => {
+          later(keys[i], setOutput);
+        })
         unorderedList.appendChild(newButton);
       }
     }, input.value);
-    later(input.value, setOutput);
   }
 });
 
